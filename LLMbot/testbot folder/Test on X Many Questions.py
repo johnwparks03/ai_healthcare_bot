31c403f141b1b75df3ac7b519fc7eb204c2d80de
BobSanders64 <58925347+BobSanders64@users.noreply.github.com>
2025-11-28 11:50:48 -0500

    Made it variable for amt of questions, default is 100

--- LLMbot/testbot folder/Test on X Many Questions.py
+++ LLMbot/testbot folder/Test on X Many Questions.py
@@ -23,7 +23,7 @@ print(f"Using device: {device}")
 conn = None
 cur = None
 
-AmtOfQuestions = 4963
+AmtOfQuestions = int(input("How many questions would you like to get (NOTE: Default is 100, so probably that)? ")
 
 try:
     conn = psycopg2.connect(
